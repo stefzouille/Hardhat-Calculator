8bbc13f724054b5c6f08e2b28764dc29b5ffdec9
stefzouille <contact.lokcy@gmail.com>
2021-05-20 19:12:23 +0200

    add

--- test/Calculator.js
+++ test/Calculator.js
@@ -1,18 +1,25 @@
-import "../contracts/Calculator.sol"
-const { expect } = require('chai');
+// import "../contracts/Calculator.sol"
+// const { expect } = require('chai');
 
-describe('Calculator', () => {
-  let Calculator, New;
-  it("shouldn't allow division by zero", async function () {
-    let error;
-    const fail = await Calculator.deploy();
-    try {
-      await fail.div(5, 0);
-    } catch (e) {
-      error = e;
-    }
+// describe('Calculator', () => {
+//   let Calculator, New;
+//   it("shouldn't allow division by zero", async function () {
+//     let error;
+//     const fail = await Calculator.deploy();
+//     try {
+//       await fail.div(5, 0);
+//     } catch (e) {
+//       error = e;
+//     }
 
-    assert.isDefined(error, "No exception thrown during divison");
-    assert.isTrue(error.message.search("revert") >= 0, "Expected transaction revert");
-  });
-});
+//     assert.isDefined(error, "No exception thrown during divison");
+//     assert.isTrue(error.message.search("revert") >= 0, "Expected transaction revert");
+//   });
+// });
+// const calculator = artifacts.require("Calculator");
+
+describe('deploy', () => {
+  it('are you deploy friend', async () => {
+
+  })
+})
